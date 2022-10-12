826a392fe2c8e5dd9fcc7dc09b6c4760b46d9a80
git86 <torrifictod@yahoo.com>
2022-10-12 20:36:24 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -8,3 +8,4 @@ Temporary commit content for 2022-09-18T20:36:24
 Temporary commit content for 2022-09-20T20:36:24
 Temporary commit content for 2022-09-25T20:36:24
 Temporary commit content for 2022-10-10T20:36:24
+Temporary commit content for 2022-10-12T20:36:24
