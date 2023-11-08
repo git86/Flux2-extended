11fcb3a26492c439ee519275e4e4031b5f18d2c6
git86 <torrifictod@yahoo.com>
2023-11-08 20:36:24 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -49,3 +49,4 @@ Temporary commit content for 2023-09-24T20:36:24
 Temporary commit content for 2023-09-25T20:36:24
 Temporary commit content for 2023-10-07T20:36:24
 Temporary commit content for 2023-11-06T20:36:24
+Temporary commit content for 2023-11-08T20:36:24
