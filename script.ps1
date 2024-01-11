c194234ee658322124cb9ed08757a90ba19f2848
git86 <torrifictod@yahoo.com>
2024-01-11 20:36:24 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -45,3 +45,4 @@ Temporary commit content for 2023-12-17T20:36:24
 Temporary commit content for 2023-12-18T20:36:24
 Temporary commit content for 2023-12-23T20:36:24
 Temporary commit content for 2024-01-03T20:36:24
+Temporary commit content for 2024-01-11T20:36:24
