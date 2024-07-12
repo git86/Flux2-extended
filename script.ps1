f5a1f06336b1b158e88cac5bf411a95a8b2d093f
git86 <torrifictod@yahoo.com>
2024-07-12 20:36:24 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -54,3 +54,4 @@ Temporary commit content for 2024-03-01T20:36:24
 Temporary commit content for 2024-03-10T20:36:24
 Temporary commit content for 2024-06-25T20:36:24
 Temporary commit content for 2024-07-09T20:36:24
+Temporary commit content for 2024-07-12T20:36:24
