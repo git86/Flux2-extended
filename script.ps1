4c9abdcd57c0989666836dcbb38fe80d5628ca3a
git86 <torrifictod@yahoo.com>
2024-10-16 20:36:24 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -60,3 +60,4 @@ Temporary commit content for 2024-09-01T20:36:24
 Temporary commit content for 2024-09-08T20:36:24
 Temporary commit content for 2024-09-18T20:36:24
 Temporary commit content for 2024-10-13T20:36:24
+Temporary commit content for 2024-10-16T20:36:24
